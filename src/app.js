111b53eb260d91f46f67428efd151e4521f2f21c
Md. Dipu <srdipu@hotmail.com>
2024-10-21 23:34:04 +0600

    [feat/home] Added a button
    Added a button with styles and added alert into event listener.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,4 @@
 document.getElementById('mainTitle').innerText = 'I changed it using javascript.';
+document.getElementsByTagName('button')[0].addEventListener('click', () => {
+    alert('Hello Everyone.');
+});
