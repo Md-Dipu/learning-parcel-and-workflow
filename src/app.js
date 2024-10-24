92162ce6b52bb254e0688a612ea24683dee35b01
Md. Dipu <srdipu@hotmail.com>
2024-10-24 23:25:25 +0600

    [chore/lint] Added automatic precommit check with eslint and prettier (#18)

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 // Udate the main title text dynamically using its element ID
-document.getElementById('mainTitle').innerText = 'I changed it using javascript.';
+const title = document.getElementById('mainTitle');
+title.innerText = 'I changed it using javascript.';
 
 // Attach a click event listener to the first button, triggerig an alert when clicked!
 document.getElementsByTagName('button')[0].addEventListener('click', () => {
-    alert('Hello Everyone.');
+  alert('Hello Everyone.');
 });
